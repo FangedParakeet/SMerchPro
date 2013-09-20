fd4e35ee3a994e1cf45880ecc1eaa44488a6e6d5
Samir Merchant <fanged.parakeet@gmail.com>
2013-09-21 01:57:15 +0530

    css clean up and nav bar prep

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -4,6 +4,6 @@ scheduler = Rufus::Scheduler.start_new
 scheduler.every '10m' do
    require "net/http"
    require "uri"
-   url = 'http://sinistereo.fm'
+   url = 'http://samirmerchant.com'
    Net::HTTP.get_response(URI.parse(url))
 end
